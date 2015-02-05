e9015ed811ec01dc7da95cc504afa1bbc1b87ef0
gravitano <gravitano16@gmail.com>
2015-02-05 10:56:46 +0700

    Fix package path

--- TrustyServiceProvider.php
+++ TrustyServiceProvider.php
@@ -4,46 +4,46 @@ use Illuminate\Support\ServiceProvider;
 
 class TrustyServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Boot the package.
-	 * 
-	 * @return void 
-	 */
-	public function boot()
-	{
-		$this->package('pingpong/trusty');
-	}
+    /**
+     * Boot the package.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('pingpong/trusty', 'trusty', __DIR__ . '/src');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['pingpong.trusty'] = $this->app->share(function($app)
-		{
-			$auth = $app['auth']->driver();
-			
-			return new Trusty($auth, $app['router']);	
-		});
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['pingpong.trusty'] = $this->app->share(function ($app)
+        {
+            $auth = $app['auth']->driver();
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('pingpong.trusty');
-	}
+            return new Trusty($auth, $app['router']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('pingpong.trusty');
+    }
 
 }
