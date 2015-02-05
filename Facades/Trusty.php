8dbb600da4de500a01a08a3f8b631bf59f1a855a
gravitano <gravitano16@gmail.com>
2015-02-05 10:58:40 +0700

    Add doc block

--- Facades/Trusty.php
+++ Facades/Trusty.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Facade;
 class Trusty extends Facade {
 
 	/**
+	 * Get the registered name of the component.
+	 *
 	 * @return string
-     */
+	 */
 	protected static function getFacadeAccessor()
 	{
 		return 'trusty';
