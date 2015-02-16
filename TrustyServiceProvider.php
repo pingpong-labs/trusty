8a6fdf765a9ecc998a673ccbe7ab761e10b4ce45
gravitano <gravitano16@gmail.com>
2015-02-16 21:25:15 +0700

    publish & merge config

--- TrustyServiceProvider.php
+++ TrustyServiceProvider.php
@@ -19,8 +19,10 @@ class TrustyServiceProvider extends ServiceProvider {
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/src/migrations/' => base_path('/database/migrations')
+            __DIR__ . '/src/migrations/' => base_path('/database/migrations'),
+            __DIR__.'/src/config/config.php' => config_path('trusty.php')
         ]);
+        $this->mergeConfigFrom(__DIR__.'/src/config/config.php', 'trusty');
     }
 
     /**
