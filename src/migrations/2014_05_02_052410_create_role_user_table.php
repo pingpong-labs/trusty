1c01b13c134ec1a3dc8bc46c1b88ae0c075e0697
gravitano <gravitano16@gmail.com>
2015-02-16 21:03:22 +0700

    Use config helper & update config key

--- src/migrations/2014_05_02_052410_create_role_user_table.php
+++ src/migrations/2014_05_02_052410_create_role_user_table.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
@@ -18,7 +19,7 @@ class CreateRoleUserTable extends Migration {
             $table->integer('role_id')->unsigned()->index();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->integer('user_id')->unsigned()->index();
-            $table->foreign('user_id')->references('id')->on(Config::get('auth.table'))->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on(config('trusty.model.user'))->onDelete('cascade');
             $table->timestamps();
         });
     }
